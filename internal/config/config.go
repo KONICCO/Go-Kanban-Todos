2f0ad7e587cb4593102b2e35ac19fbaeafd10419
KONICCO <nico678514@gmail.com>
2024-07-27 23:59:30 +0700

    error confix

--- internal/config/config.go
+++ internal/config/config.go
@@ -22,7 +22,7 @@ type ServerConfig struct {
 
 func New() *Config {
 	// Load .env file
-	err := godotenv.Load("../../.env")
+	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Fatalf("Error loading .env file %v", err)
 	}
